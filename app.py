69322653df4f43a6878eb5c513dcfce2cfdc21ea
Arnav-Sharma-3 <arnavsharma110305@gmail.com>
2025-07-25 23:10:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -189,7 +189,15 @@ if uploaded_file:
                     file_name="magnetic_fields_results.csv",
                     mime="text/csv"
                 )
-
+st.markdown("---")
+st.markdown(
+    "📌 The cosmology calculator used for this project is based on [James Schombert's python version of the Ned Wright's Cosmology Calculator](https://www.astro.ucla.edu/~wright/CC.python).",
+    unsafe_allow_html=True
+)
+st.markdown(
+    "📖 Reference: Wright, E. L. (2006). A Cosmology Calculator for the World Wide Web. *Publications of the Astronomical Society of the Pacific*, 118(850), 1711–1715. [doi:10.1086/510102](https://doi.org/10.1086/510102)",
+    unsafe_allow_html=True
+)
 st.markdown(
     """
     <hr style="margin-top: 3rem; margin-bottom: 1rem;">
